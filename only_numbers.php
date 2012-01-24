4b38a13962d0af5c28d793b00794a5821a360e34
Rodrigo <rodurma@gmail.com>
2012-01-24 17:40:30 -0200

    add only_numbers function

---
+++ only_numbers.php
@@ -0,0 +1,9 @@
+<?php
+
+function only_numbers($number){
+    return preg_replace("/[^[:digit:]]/", "", $number);
+}
+
+echo only_numbers("Em 2011 eu fui com 3 amigos para uma festa que tinha 300 pessoas");
+
+?>
