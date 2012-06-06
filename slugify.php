855355b1fabc3179c8f55d7af36701213e8104ed
Rodrigo Maia <rodurma@gmail.com>
2012-06-06 17:34:45 -0300

    Add slugify function

---
+++ slugify.php
@@ -0,0 +1,28 @@
+<?php
+// by Maerlyn
+// source: http://stackoverflow.com/questions/2955251/php-function-to-make-slug-url-string
+function slugify($text)
+{ 
+  // replace non letter or digits by -
+  $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+  // trim
+  $text = trim($text, '-');
+
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  // lowercase
+  $text = strtolower($text);
+
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+
+  if (empty($text))
+  {
+    return 'n-a';
+  }
+
+  return $text;
+}
+?>
