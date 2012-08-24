e01d90469491ea532626a04452686eb68c8db47c
Rodrigo Maia <rodurma@gmail.com>
2012-08-24 17:37:55 -0300

    testando hooks

--- only_numbers.php
+++ only_numbers.php
@@ -4,6 +4,6 @@ function only_numbers($number){
     return preg_replace("/[^[:digit:]]/", "", $number);
 }
 
-echo only_numbers("Em 2011 eu fui com 3 amigos para uma festa que tinha 300 pessoas");
+echo only_numbers("Em 2011 eu fui com 3 amigos para uma festa que tinha 300 pessoas"); 
 
 ?>
